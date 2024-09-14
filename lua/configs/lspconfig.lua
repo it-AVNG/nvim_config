8a536852df8756d42d1732420143424d74e58a8a
AVNG <avng@MSI>
2024-09-14 21:32:19 +0700

    add pyright to lsp servers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "lua_ls", "basedpyright", "ruff_lsp", "eslint" }
+local servers = { "html", "cssls", "lua_ls", "pyright", "ruff_lsp", "basedpyright", "eslint" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
