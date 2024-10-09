b1b8d97738613bebbc79af4f3b6bf6f5255e0186
AVNG <it.avng@gmail.com>
2024-10-09 09:53:27 +0700

    change theme dialogue window style to flat

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("n", "<leader>th", function ()
+  require("nvchad.themes").open({style = "flat"})
+end)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
