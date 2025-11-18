e4166450cbcc937ffd2b7ee7b9b5e08a11e84608
An Vu Nguyen <it.avng@gmail.com>
2025-11-18 08:39:12 +0700

    build: update config for snacks, add animation and status line folding marks

--- lua/avng/plugins/snacks.lua
+++ lua/avng/plugins/snacks.lua
@@ -1,35 +1,15 @@
--- return { "folke/snacks.nvim", priority = 1000, lazy = false,
---   ---@type snacks.Config
---   opts = {
---     -- your configuration comes here or leave it empty to use the default
---     -- settings refer to the configuration section below
---     animate = { enabled = true },
---     -- lazygit={ enabled = true }, 
---     bigfile = {enabled = true },
---     dashboard = { enabled = true },
---     explorer = { enabled =true },
---     indent = { enabled = true },
---     input = { enabled = true }, 
---     picker = { enabled = true },
---     notifier = { enabled = true },
---     quickfile = { enabled = true },
---     scope = { enabled = true },
---     scroll = { enabled = true },
---     statuscolumn = { enabled = true },
---     words = { enabled = true },
---   },
---   keys = {
---     { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
---     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
---   },
--- }
---
 return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
   ---@type snacks.Config
   opts = {
+    animate = {
+      duration = 20, -- ms per step
+      easing = "linear",
+      fps = 120,
+      enabled = true
+    },
     bigfile = { enabled = true },
     dashboard = { enabled = true },
     explorer = { enabled = true },
@@ -43,7 +23,22 @@ return {
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
-    statuscolumn = { enabled = true },
+    keymap = { enabled = true },
+    statuscolumn = {
+        left = { "mark", "sign" }, -- priority of signs on the left (high to low)
+        right = { "fold", "git" }, -- priority of signs on the right (high to low)
+        folds = {
+          open = true, -- show open fold icons
+          git_hl = true, -- use Git Signs hl for fold icons
+        },
+        git = {
+          -- patterns to match Git signs
+          patterns = { "GitSign", "MiniDiffSign" },
+        },
+        refresh = 50, -- refresh at most every 50ms
+
+      enabled = true,
+    },
     words = { enabled = true },
     styles = {
       notification = {
